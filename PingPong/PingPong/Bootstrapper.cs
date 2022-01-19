ac98634af180d1c52c11a15d59337978a1a3b7e6
iTz1T4Y <itz1t4y@gmail.com>
2022-01-19 17:20:51 +0200

    Created solution ClientCore

--- PingPong/PingPong/Bootstrapper.cs
+++ PingPong/PingPong/Bootstrapper.cs
@@ -1,8 +1,11 @@
-﻿using ServerCore;
+﻿using ClientCore;
+using ClientImplementations;
+using ServerCore;
 using ServerImplementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +17,21 @@ namespace PingPong
 
         public void Start()
         {
+            Console.WriteLine("===Server===");
             Console.WriteLine("Please enter the desired port");
-            int port = int.Parse(Console.ReadLine());
+            int serverPort = int.Parse(Console.ReadLine());
             IDataProcessor dataProcessor = new StringDataProcessor();
             IConnectionHandler<Socket> connectionHandler = new SocketConnectionHandler(dataProcessor);
-            Server server = new Server(port, connectionHandler);
-            server.Start();
+            Server server = new Server(serverPort, connectionHandler);            
+            Task serverTask = new Task(() =>server.Start());
+
+            Console.WriteLine("===Client===");
+            Console.WriteLine("Please enter the desired IP");
+            IPAddress serverIp = IPAddress.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter the desired port");
+            int clientPort = int.Parse(Console.ReadLine());
+            Client<string> client = new Client<string>(serverIp, clientPort, new StringInputGetter(), new StringDataConverter());
+            client.Run();
         }
 
     }
