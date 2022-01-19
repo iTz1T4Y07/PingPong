b48d8b3eefd38be81c253aafcf962b8ad1ad7eca
iTz1T4Y <itz1t4y@gmail.com>
2022-01-19 16:11:50 +0200

    Added StringDataProcessor

--- PingPong/ServerImplementations/StringDataProcessor.cs
+++ PingPong/ServerImplementations/StringDataProcessor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServerCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace ServerImplementations
 {
-    public class StringDataProcessor
+    public class StringDataProcessor : IDataProcessor
     {
+        public byte[] GetDataToReturn(byte[] receivedData)
+        {
+            Console.WriteLine($"Received [{Encoding.ASCII.GetString(receivedData)}]");
+            return receivedData;
+        }
     }
 }
