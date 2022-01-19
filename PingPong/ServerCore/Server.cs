5e355ff8a8ccc6ff7a44a97552034e28c881a3e4
iTz1T4Y <itz1t4y@gmail.com>
2022-01-19 15:11:25 +0200

    Added ctor to server

--- PingPong/ServerCore/Server.cs
+++ PingPong/ServerCore/Server.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,15 @@ namespace ServerCore
 {
     class Server
     {
+        private Socket _serverSocket;
+
+        private IPEndPoint _socketConfiguration;
+
+        public Server(int port)
+        {            
+            _serverSocket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            _socketConfiguration = new IPEndPoint(IPAddress.Any, port);
+            _serverSocket.Bind(_socketConfiguration);
+        }
     }
 }
