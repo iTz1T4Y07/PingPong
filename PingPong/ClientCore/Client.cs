d59181e072ed9283cedde85072fa67c40e6e3d77
iTz1T4Y <itz1t4y@gmail.com>
2022-01-19 16:38:52 +0200

    Implemented Client ctor

--- PingPong/ClientCore/Client.cs
+++ PingPong/ClientCore/Client.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClientCore
 {
-    public class Client
+    public class Client<T>
     {
+        private Socket _socket;
+
+        IGetInput<T> _inputGetter;
+
+        IDataConvert<T> _dataConverter;
+
+        public Client(IPAddress ip, int port, IGetInput<T> inputGetter, IDataConvert<T> dataConverter)
+        {
+            _socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            _socket.Connect(ip, port);
+
+            _inputGetter = inputGetter;
+            _dataConverter = dataConverter;
+        }
     }
 }
