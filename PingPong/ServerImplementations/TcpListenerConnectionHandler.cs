3cf8b97bfe3071aac4006e407c4d0cf404e6eb51
iTz1T4Y <itz1t4y@gmail.com>
2022-01-20 08:40:00 +0200

    Implemented ctor of private IDataProcessor _dataProcessor;

--- PingPong/ServerImplementations/TcpListenerConnectionHandler.cs
+++ PingPong/ServerImplementations/TcpListenerConnectionHandler.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using ServerCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServerImplementations
 {
-    public class TcpListenerConnectionHandler
+    public class TcpListenerConnectionHandler : IConnectionHandler<TcpClient>
     {
+        private IDataProcessor _dataProcessor;
+
+        public TcpListenerConnectionHandler(IDataProcessor dataProcessor)
+        {
+            _dataProcessor = dataProcessor;
+        }
+
+        public Task HandleNewConnection(TcpClient clientSocket)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
