72d02aa4c71c4842cd4df3d1ee9d1da7f42deca4
iTz1T4Y <itz1t4y@gmail.com>
2022-01-20 09:14:28 +0200

    Implemented PersonObjectProcessor.GetDateToReturn()

--- PingPong/ServerImplementations/PersonObjectProcessor.cs
+++ PingPong/ServerImplementations/PersonObjectProcessor.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using PingPong.Common;
+using ServerCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ServerImplementations
 {
-    public class PersonObjectProcessor
+    public class PersonObjectProcessor : IDataProcessor
     {
+        public byte[] GetDataToReturn(byte[] receivedData)
+        {
+            string jsonString = Encoding.ASCII.GetString(receivedData);
+            Person personReceived;
+            try
+            {
+                personReceived = JsonSerializer.Deserialize<Person>(jsonString);
+            }
+            catch(JsonException e)
+            {
+                Console.WriteLine($"Failed to deserialize person. {e}");
+                return new byte[1] { 0 };
+                //Change Console to log
+            }
+
+            Console.WriteLine($"Received new person! [{personReceived.ToString()}]");
+            return receivedData;
+        }
     }
 }
