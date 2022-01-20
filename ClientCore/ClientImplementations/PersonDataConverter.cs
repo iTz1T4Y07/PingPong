a54de7bd32822399e0147817bec3f0eb6b0cfbb2
iTz1T4Y <itz1t4y@gmail.com>
2022-01-20 09:39:45 +0200

    Implemented PersonDataConverter

--- ClientCore/ClientImplementations/PersonDataConverter.cs
+++ ClientCore/ClientImplementations/PersonDataConverter.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using ClientCore;
+using PingPong.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ClientImplementations
 {
-    public class PersonDataConverter
+    public class PersonDataConverter : IDataConvert<Person>
     {
+        public byte[] Convert(Person dataToConvert)
+        {
+            string jsonString;
+            try
+            {
+                jsonString = JsonSerializer.Serialize(dataToConvert);
+            }
+            catch (NotSupportedException e)
+            {
+                Console.WriteLine($"Couldn't serialize person object. {e}");
+                //Change console to logger
+                return new byte[] { 0 };
+            }
+
+            return Encoding.ASCII.GetBytes(jsonString);
+        }
     }
 }
