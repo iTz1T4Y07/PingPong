da45909d18f5480111da1cdd3637b5575a47cc9d
iTz1T4Y <itz1t4y@gmail.com>
2022-01-20 08:26:20 +0200

    Implemented Person.cs

--- PingPong/PingPong.Common/Person.cs
+++ PingPong/PingPong.Common/Person.cs
@@ -8,5 +8,19 @@ namespace PingPong.Common
 {
     public class Person
     {
+        private string _name;
+
+        private int _age;
+
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name} is {_age} years old.";
+        }
     }
 }
