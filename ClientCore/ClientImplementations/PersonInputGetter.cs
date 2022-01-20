7f482e2926682b8ecadb677c5716d2dbb1d00e91
iTz1T4Y <itz1t4y@gmail.com>
2022-01-20 09:43:31 +0200

    Implemented PersonInputGetter

--- ClientCore/ClientImplementations/PersonInputGetter.cs
+++ ClientCore/ClientImplementations/PersonInputGetter.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ClientCore;
+using PingPong.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,19 @@ using System.Threading.Tasks;
 
 namespace ClientImplementations
 {
-    public class PersonInputGetter
+    public class PersonInputGetter : IGetInput<Person>
     {
+        public Person GetInput()
+        {
+            Console.WriteLine("Please enter person name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Please enter {name}'s age");
+            int age;
+            if (!int.TryParse(Console.ReadLine(), out age))
+            {
+                return null;
+            }
+            return new Person(name, age);
+        }
     }
 }
